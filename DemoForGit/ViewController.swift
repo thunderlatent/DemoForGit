b01407d54412ed2db740e317f51a1d3378b1b725
Jimmy Chen <jinny1993820@gmail.com>
2020-09-25 15:59:26 +0800

    新增Hello world

---
+++ DemoForGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoForGit
+//
+//  Created by Jimmy on 2020/9/25.
+//  Copyright © 2020 yuming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
